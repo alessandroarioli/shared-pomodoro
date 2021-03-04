2493fcf37eecc0e512f546280e5e3d2a5284ed03
aarioli <alessandro.arioli@lastminute.com>
2021-03-04 12:24:46 +0100

    Move the build folder in the server directory

--- server/server.js
+++ server/server.js
@@ -6,11 +6,11 @@ const express = require("express");
 const app = express(); // create express app
 
 // add middlewares
-app.use(express.static(path.join(__dirname, "..", "build")));
+app.use(express.static(path.join(__dirname, "build")));
 app.use(express.static("public"));
 
 app.use((req, res, next) => {
-    res.sendFile(path.join(__dirname, "..", "build", "index.html"));
+    res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 // Websockets
